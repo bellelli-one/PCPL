e21d50ce970995d94e019fbe28f97d2aa9c91edd
bellelli <ss7532159@yandex.ru>
2024-10-24 18:08:22 +0300

    to pdf

--- venv2/Labs/Lab2/lab_python_fp/gen_random.py
+++ venv2/Labs/Lab2/lab_python_fp/gen_random.py
@@ -1,5 +1,14 @@
-import random
+# import random
 
-def gen_random(c, s, e):
-    vals = [random.randint(s, e) for _ in range(c)]
-    yield vals
+# def gen_random(c, s, e):
+#     vals = [random.randint(s, e) for _ in range(c)]
+#     yield vals
+
+def fib(n):
+    a = 0
+    b = 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+print(list(fib(10)))
